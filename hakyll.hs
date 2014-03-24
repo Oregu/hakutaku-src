064130b373de52e76ba3f00fd0e0f4a199235e96
Oregu <oleg.burykin@gmail.com>
2014-03-24 18:18:18 +0400

    Decompose index

--- hakyll.hs
+++ hakyll.hs
@@ -20,6 +20,12 @@ main = hakyll $ do
         route   idRoute
         compile compressCssCompiler
 
+    match "root/index.html" $ do
+        route   $ constRoute "index.html"
+        compile $ getResourceBody
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
         compile $ pandocCompiler
@@ -47,21 +53,19 @@ main = hakyll $ do
                 >>= loadAndApplyTemplate "templates/default.html" archiveCtx
                 >>= relativizeUrls
 
-    match "index.html" $ do
-        route idRoute
-        compile $ do
-            copyFileCompiler
-            getResourceBody >>= relativizeUrls
-        --    posts <- recentFirst =<< loadAll "posts/*"
-        --    let indexCtx =
-        --            listField "posts" postCtx (return posts) `mappend`
-        --            constField "title" "Home"                `mappend`
-        --            defaultContext
+    --match "index.html" $ do
+    --    route idRoute
+    --    compile $ do
+    --        posts <- recentFirst =<< loadAll "posts/*"
+    --        let indexCtx =
+    --                listField "posts" postCtx (return posts) `mappend`
+    --                constField "title" "Home"                `mappend`
+    --                defaultContext
 
-        --    getResourceBody
-        --        >>= applyAsTemplate indexCtx
-        --        >>= loadAndApplyTemplate "templates/default.html" indexCtx
-        --        >>= relativizeUrls
+    --        getResourceBody
+    --            >>= applyAsTemplate indexCtx
+    --            >>= loadAndApplyTemplate "templates/default.html" indexCtx
+    --            >>= relativizeUrls
 
     match "templates/*" $ compile templateCompiler
 
